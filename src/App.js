e847fb1a93f6c8b1a85e7af9056c1fd1478f725a
lucra <kwanay1010@naver.com>
2022-07-30 01:40:50 +0900

    그냥 중간저장

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {ReactComponent as Logo} from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
+import {Nav,Container,Row,Col} from 'react-bootstrap';
+import data from './data';
 
 function App() {
+  console.log(data.length)
   return (
     <>
       
@@ -32,6 +34,19 @@ function App() {
           </Nav.Link>
         </Nav.Item>
       </Nav>
+
+      <div className="container">
+        <div className="row">
+          {data.map((d,i)=>{
+            return(
+              <div className="col-md-4">
+                <img src={`../img/${i}.jpg`} width="80%" />
+                <h4>{d.title}</h4>
+                <p>{d.date}</p>
+              </div>
+          ) })}
+        </div>
+      </div>
       </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+let data = [
+    {
+      id : 0,
+      title : "단체샷 1",
+      date : "Born in France",
+    },
+  
+    {
+      id : 1,
+      title : "단체샷 1",
+      date : "Born in Seoul",
+    },
+  
+    {
+      id : 2,
+      title : "암장사진",
+      date : "Born in the States",
+    }
+  ] 
+
+
+  export default data;
